8410353db96167350981396075cb9c80492eafe1
Javier <jjmm7@live.com>
2018-11-26 19:36:38 -0500

    Semana6

--- Proyecto4/Controllers/HomeController.cs
+++ Proyecto4/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Proyecto4.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,10 +10,15 @@ namespace Proyecto4.Controllers
 {
 	public class HomeController : Controller
 	{
-		#region Methods
+        LibroVisitas libro = null;
+        SqlParameter[] parParameter = null;
 
-		// GET: Home
-		public ActionResult Index()
+
+
+        #region Methods
+
+        // GET: Home
+        public ActionResult Index()
 		{
 			return View();
 		}
@@ -24,12 +30,50 @@ namespace Proyecto4.Controllers
 
 		public ActionResult CargaDatos()
 		{
-			string nombre = Request.Form["nombre"].ToString();
+            /*string nombre = Request.Form["nombre"].ToString();
 			string comentarios = Request.Form["comentarios"].ToString();
 			LibroVisitas libro = new LibroVisitas();
 			libro.Grabar(nombre, comentarios);
-			return View();
-		}
+			return View();*/
+
+            string nombre = Request["nombre"].ToString();
+            string comentario = Request["comentarios"].ToString();
+
+            int opc = 2;
+
+            try
+            {
+                libro = new LibroVisitas();
+                parParameter = new SqlParameter[3];
+
+                parParameter[0] = new SqlParameter();
+                parParameter[0].ParameterName = "@opc";
+                parParameter[0].SqlDbType = System.Data.SqlDbType.Int;
+                parParameter[0].SqlValue = opc;
+
+                parParameter[1] = new SqlParameter();
+                parParameter[1].ParameterName = "@nombre";
+                parParameter[1].SqlDbType = System.Data.SqlDbType.VarChar;
+                parParameter[1].Size = 50;
+                parParameter[1].SqlValue = nombre;
+
+                parParameter[2] = new SqlParameter();
+                parParameter[2].ParameterName = "@comentario";
+                parParameter[2].SqlDbType = System.Data.SqlDbType.VarChar;
+                parParameter[2].Size = 500;
+                parParameter[2].SqlValue = comentario;
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            libro.EjecutarSP(parParameter, "sp_comentario");
+            return View();
+        }
 
 		public ActionResult ListadoVisitas()
 		{
@@ -38,6 +82,9 @@ namespace Proyecto4.Controllers
 			ViewData["libro"] = todo;
 			return View();
 		}
-		#endregion
-	}
+        #endregion
+
+
+       
+    }
 }
--- Proyecto4/Models/LibroVisitas.cs
+++ Proyecto4/Models/LibroVisitas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,48 @@ namespace Proyecto4.Models
 {
 	public class LibroVisitas
 	{
+        SqlConnection cnnConexion = null;
+        SqlCommand cmdComando = null;
+
+        string strCadenaConexion = "";
+
+        public LibroVisitas()
+        {
+            strCadenaConexion = @"Data Source=SMLAB304PC02_18;Initial Catalog=mvc_web_comentariodb;Integrated Security=False;User ID=sa;Password=Uisrael2018;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
+
+
+        public void EjecutarSP(SqlParameter[] parParameter, string nombreSP)
+        {
+            try
+            {
+                cnnConexion = new SqlConnection(strCadenaConexion);
+
+                cmdComando = new SqlCommand();
+
+                cmdComando.Connection = cnnConexion;
+                cnnConexion.Open();
+
+                cmdComando.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmdComando.CommandText = nombreSP;
+
+                cmdComando.Parameters.AddRange(parParameter);
+
+                cmdComando.ExecuteNonQuery();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                cnnConexion.Dispose();
+                cmdComando.Dispose();
+            }
+        }
 
 		#region Methods
 
